86e22cffb196f61733a9fa92e9ed5a84fb308214
Leonel Nieto <leonelnieto@gmail.com>
2020-04-11 08:40:25 -0600

    Just messing around

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { request } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
@@ -13,10 +13,15 @@ app.use(morgan('combined'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header("Allow-Control-Allow-Origin", process.env.ORIGIN || "*");
+  next();
+})
 
 app.use('/city', routes.city);
 app.use('/zip', routes.zip);
 app.use('/map', routes.map);
+app.use('/', (req, res) => res.send("Welcome to a simple api proxy"));
 
 
 app.listen(process.env.PORT, () => {
